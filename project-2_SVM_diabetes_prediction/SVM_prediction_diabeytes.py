ee54246964118eca5ccee58d87c27f9917f555a5
DevaNandanJS <devanandan.connect@gmail.com>
2026-06-11 14:47:36 +0530

    new project 2 start

---
+++ project-2_SVM_diabetes_prediction/SVM_prediction_diabeytes.py
@@ -0,0 +1,14 @@
+# importing libraries
+import numpy as np
+import panas as pd
+import matplotlip.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import standardscaler
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+#loading the dataset
+diabetes_dataset= pd.read_csv("datasets/diabetes.csv")
+
+
+#EDA
